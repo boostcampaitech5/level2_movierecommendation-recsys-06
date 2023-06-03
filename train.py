902fc9ea10eb3f447b87ebca2e8a46b03ceccf44
junhyuk1229 <junhyuk1229@gmail.com>
2023-06-03 19:45:13 +0900

    [Feature] Added data loading
    Signed-off-by: junhyuk1229 <junhyuk1229@gmail.com>

--- train.py
+++ train.py
@@ -1,10 +1,13 @@
-from utils.util import get_settings
+from utils.util import get_settings, get_raw_data
 
 
 def main() -> None:
     settings: dict = get_settings()
 
+    raw_data = get_raw_data(settings)
+
     return
+
 
 if __name__ == "__main__":
     main()
--- utils/util.py
+++ utils/util.py
@@ -1,15 +1,35 @@
+import os
+import pandas as pd
 import yaml
 
 
 def get_settings() -> dict:
-    '''
+    """
     Imports settings from yaml file.
 
     Returns:
         setting_dict(dict): Dictionary containing settings
-    '''
+    """
 
     with open("config.yaml", "r") as f:
         setting_dict: dict = yaml.safe_load(f)
-    
+
     return setting_dict
+
+
+def get_raw_data(settings) -> pd.DataFrame:
+    """
+    Gets raw data from csv file.
+
+    Parameters:
+        settings(dict): Contains settings
+    Returns:
+        raw_input_df(pd.DataFrame): Raw data file
+    """
+
+    data_path: str = settings["path"]["data"]
+    df_path: str = settings["input_data"]
+
+    raw_input_df: pd.DataFrame = pd.read_csv(os.path.join(data_path, df_path))
+
+    return raw_input_df
