648440b8bca148394d50b248fea8e3f8a06eeb82
GijeongKim98 <kkj981814@gmail.com>
2023-06-04 14:33:06 +0000

    [Fix] model_mlp_py: Modify as requested by PR

--- model/models/model_mlp.py
+++ model/models/model_mlp.py
@@ -5,17 +5,23 @@ import torch.nn as nn
 class MultiLayerPerceptron(nn.Module):
     def __init__(self, setting: dict):
         super(MultiLayerPerceptron, self).__init__()
-        input_dim = settings["mlp"]["input_dim"]
-        hidden_dims = setting["mlp"]["hidden_dims"]
-        output_dim = setting["mlp"]["output_dim"]  # == The number of movie? #
+        self.input_dim = setting["mlp"]["input_dim"]
+        self.hidden_dims = setting["mlp"]["hidden_dims"]
+        self.output_dim = setting["mlp"]["output_dim"]  # == The number of movie? #
 
-        self.sequential_linear = nn.Sequential()
+        self.module_list = nn.ModuleList()
+
+        temp_layer = self.input_dim
 
         for hidden_dim in self.hidden_dims:
-            self.sequential_linear.append(nn.Linear(input_dim, hidden_dim), nn.ReLU())
-            input_dim = hidden_dim
+            self.module_list.append(nn.Linear(temp_layer, hidden_dim))
+            self.module_list.append(nn.ReLU())
+            temp_layer = hidden_dim
 
-        self.sequential_linear.append(nn.Linear(input_dim, output_dim), nn.Sigmoid())
+        self.module_list.append(nn.Linear(temp_layer, self.output_dim))
+        self.module_list.append(nn.Sigmoid())
+
+        self.sequential_linear = nn.Sequential(*self.module_list)
 
     def forword(x):
         output = self.sequential_linear(x)
